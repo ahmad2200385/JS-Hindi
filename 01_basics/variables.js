992cffcd89ebc70dc0966b6db0f08214d4dd4ae7
ahmad2200385 <143064391+ahmad2200385@users.noreply.github.com>
2026-01-16 09:59:05 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 445637;
+let accountEmail = "ahmad@gmail.com";
+var accountPassword = "1234567";
+accountCity = "Bihar";
+let accountState;
+
+//accountId = 2;
+
+accountEmail = "raza@gmail.com"
+accountPassword = "78753"
+accountCity = "Punjab"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
