b0aee0cf6a7c37ca1ef3c397449322f950ede527
ahmad2200385 <143064391+ahmad2200385@users.noreply.github.com>
2026-01-22 10:03:37 +0000

    work on iife

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,56 @@
+
+let a = 300
+
+if(true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    // console.log("INNNER", a);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one() {
+    const username = "ahmad"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    
+    // two()
+}
+// one()
+
+
+
+
+if(true)  {
+    const username = "ahmad"
+    if(username === "ahmad") {
+        const website = " youtube"
+        // console.log(username+website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// console.log(addOne(5))
+function addOne(num) {
+    return num + 1
+}
+
+
+
+// console.log(addTwo(5))
+const addTwo = function(num) {
+    return num + 2
+}
