2f759fa70f91559882a5c367b8915fc6b82d184f
ahmad2200385 <ahmadrazabxr5643@gmail.com>
2026-05-12 23:26:31 +0530

    learn about refrence values and spread operator

--- 04_control_flow/two.js
+++ 04_control_flow/two.js
@@ -12,3 +12,30 @@ const newNum = number.map ( (num) => num * 10)
                      .filter ( (num) => num >= 40 )
 
 console.log(newNum);
+
+
+
+// How to copy reference values
+
+// var a = [1, 2, 3];
+
+// var b = a; // b is a reference to the same array as a
+
+// b.pop(); // This will modify the array that both a and b reference
+
+// spread operator
+
+var a = [1, 2, 3];
+
+var b = [...a]; // b is a new array that is a copy of a
+
+b.pop(); // This will only modify b, not a
+
+var obj = { name: 'Alice', age: 30 };
+
+var objCopy = { ...obj }; // objCopy is a new object that is a copy of obj
+
+objCopy.name = 'Bob'; // This will only modify objCopy, not obj
+
+
+//falsey values: 0, '', null, undefined, NaN, false , document.all
